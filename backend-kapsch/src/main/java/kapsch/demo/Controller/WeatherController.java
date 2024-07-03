5e30d32c9674d576b95e35392698835a620227c4
Cristian Carrasco <crxcarrasco@gmail.com>
2024-07-03 18:47:58 -0400

    order folder front

--- backend-kapsch/src/main/java/kapsch/demo/Controller/WeatherController.java
+++ backend-kapsch/src/main/java/kapsch/demo/Controller/WeatherController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import kapsch.demo.Entities.WeatherDetail;
 import kapsch.demo.Services.WeatherService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")  // Permitir solicitudes CORS desde localhost:4200
 public class WeatherController {
 
     private final WeatherService weatherService;
