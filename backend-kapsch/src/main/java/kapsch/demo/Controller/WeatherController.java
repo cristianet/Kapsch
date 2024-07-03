66cefea9caa8920fccddc7a37ac38d87eede3f84
Cristian Carrasco <crxcarrasco@gmail.com>
2024-07-03 13:10:18 -0400

    add db details

--- backend-kapsch/src/main/java/kapsch/demo/Controller/WeatherController.java
+++ backend-kapsch/src/main/java/kapsch/demo/Controller/WeatherController.java
@@ -1,14 +1,17 @@
 package kapsch.demo.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kapsch.demo.Entities.WeatherRequest;
+import kapsch.demo.Entities.WeatherDetail;
 import kapsch.demo.Services.WeatherService;
 
 @RestController
@@ -37,4 +40,19 @@ public class WeatherController {
         List<WeatherRequest> weatherRequests = weatherService.getAllWeatherRequests();
         return ResponseEntity.ok(weatherRequests);
     }
+    
+    
+    @GetMapping("/weather-details")
+    public ResponseEntity<List<WeatherDetail>> getAllDetailsRequests() {
+        List<WeatherDetail> weatherDetails = weatherService.getAllWeatherDetails();
+        return ResponseEntity.ok(weatherDetails);
+    }
+    
+    @GetMapping("/weather-details/{id}")
+    public ResponseEntity<WeatherDetail> getWeatherDetailById(@PathVariable Long id) {
+        Optional<WeatherDetail> weatherDetail = weatherService.getWeatherDetailsById(id);
+            
+    return weatherDetail.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
 }
